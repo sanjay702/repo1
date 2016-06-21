1c5d5693a5f133d3ff8d9f57e28a09dc853f50ad
Sanjay702 <k.sanjay2@tcs.com>
2016-06-21 09:16:38 -0400

    frst commit

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args){
+            var c = new CalculatorLib.Class1();
+            var result = c.sum(10,20);
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        
+        }
+    }
+}
